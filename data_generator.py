eac0400753249547ea460b20302690bfbf570dde
Julian Rice <julianrice@dotcomjungle.com>
2021-10-12 17:04:52 -0700

    more data generator outlining

--- data_generator.py
+++ data_generator.py
@@ -1,12 +1,20 @@
+import glob, re, sys
+from pathlib import PurePath
+
 import tensorflow as tf
 from tensorflow import keras
 
 import numpy as np
+import librosa
 
-class DataGenerator(keras.utils.Sequence):
+class SpectogramGenerator(keras.utils.Sequence):
 
-    def __init__(self, ):
-        ...
+    def __init__(self, ids, dur_step=0.2):
+        self.ids = ids
+        self.dur_step = dur_step
+        # TODO generate set of all possible 0.2-second non-overlapping windows
+        # of all files listed in 'ids'
+        # ex: [("01_id1...", 0.0), ("01_id1...", 0.2), ("01_id1...", 0.4), ...]
 
     def __len__(self):
         """
@@ -22,3 +30,30 @@ class DataGenerator(keras.utils.Sequence):
 
     def on_epoch_end(self):
         ...
+
+
+
+def get_generators(val_split=0.1, **kwargs):
+    """
+    create train, validation, and test generators
+    test set is all files created by player #05
+    validation set is a random split of the rest
+    args:
+        val_split: fraction out of 1
+        **kwargs: passed to the generator's init
+    """
+    files = glob.glob("guitarset/audio_mono-mic/*_mic.wav")
+    ids = [PurePath(i).stem.strip("_mic") for i in files]
+    val_size = int(len(ids) * val_split)
+    test_ids = np.array([i for i in ids if i.startswith("05")])
+    train_ids = np.array([i for i in ids if not i.startswith("05")])
+    np.random.shuffle(train_ids)
+    val_ids, train_ids = train_ids[:val_size], train_ids[val_size:]
+    
+    train_gen = SpectogramGenerator(train_ids, **kwargs)
+    val_gen = SpectogramGenerator(val_ids, **kwargs)
+    test_gen = SpectogramGenerator(test_ids, **kwargs)
+
+    return train_gen, val_gen, test_gen
+
+get_generators()
--- models.py
+++ models.py
@@ -8,53 +8,26 @@ def tio_model(input_shape):
     """
     https://towardsdatascience.com/audio-to-guitar-tab-with-deep-learning-d76e12717f81
     """
-    model_in = Input(shape = input_shape)
-    conv1 = Conv2D(32, kernel_size = (3, 3), activation = 'relu')(model_in)
-    conv2 = Conv2D(64, kernel_size = (3, 3), activation = 'relu')(conv1)
-    conv3 = Conv2D(64, kernel_size = (3, 3), activation = 'relu')(conv2)
-    pool1 = MaxPooling2D(pool_size = (2, 2), strides = (2, 2))(conv3)
+    model_in = Input(shape=input_shape)
+
+    conv1 = Conv2D(32, kernel_size=(3, 3), activation='relu')(model_in)
+    conv2 = Conv2D(64, kernel_size=(3, 3), activation='relu')(conv1)
+    conv3 = Conv2D(64, kernel_size=(3, 3), activation='relu')(conv2)
+
+    pool1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv3)
     flat = Flatten()(pool1)
 
-    y1 = Dense(152, activation = 'relu')(flat)
-    y1 = Dropout(0.5)(y1)
-    y1 = Dense(76)(y1)
-    y1 = Dropout(0.2)(y1)
-
-    y2 = Dense(152, activation = 'relu')(flat)
-    y2 = Dropout(0.5)(y2)
-    y2 = Dense(76)(y2)
-    y2 = Dropout(0.2)(y2)
-
-    y3 = Dense(152, activation = 'relu')(flat)
-    y3 = Dropout(0.5)(y3)
-    y3 = Dense(76)(y3)
-    y3 = Dropout(0.2)(y3)
-
-    y4 = Dense(152, activation = 'relu')(flat)
-    y4 = Dropout(0.5)(y4)
-    y4 = Dense(76)(y4)
-    y4 = Dropout(0.2)(y4)
-
-    y5 = Dense(152, activation = 'relu')(flat)
-    y5 = Dropout(0.5)(y5)
-    y5 = Dense(76)(y5)
-    y5 = Dropout(0.2)(y5)
-
-    y6 = Dense(152, activation = 'relu')(flat)
-    y6 = Dropout(0.5)(y6)
-    y6 = Dense(76)(y6)
-    y6 = Dropout(0.2)(y6)
-
-    # Connect heads to final output layer
-    out1 = Dense(19, activation = 'softmax', name = 'estring')(y1)
-    out2 = Dense(19, activation = 'softmax', name = 'Bstring')(y2)
-    out3 = Dense(19, activation = 'softmax', name = 'Gstring')(y3)
-    out4 = Dense(19, activation = 'softmax', name = 'Dstring')(y4)
-    out5 = Dense(19, activation = 'softmax', name = 'Astring')(y5)
-    out6 = Dense(19, activation = 'softmax', name = 'Estring')(y6)
+    outputs = []
+    for string in "EADGBe":
+        x = Dense(152, activation='relu')(flat)
+        x = Dropout(0.5)(x)
+        x = Dense(76)(x)
+        x = Dropout(0.2)(x)
+        out = Dense(19, activation='softmax', name=string+'-string')(x)
+        outputs.append(out)
 
     # Create model
-    model = Model(inputs = model_in, outputs = [out1, out2, out3, out4, out5, out6])
+    model = Model(inputs=model_in, outputs=outputs)
 
     loss = ['categorical_crossentropy'] * 6
     metrics = ['accuracy']
