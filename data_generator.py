9a5239ff59599cd1f933b9b7e1dd2829fed9e84e
Julian Rice <julianrice@dotcomjungle.com>
2021-10-14 15:53:47 -0700

    updates to data generator

--- data_generator.py
+++ data_generator.py
@@ -1,35 +1,60 @@
-import glob, re, sys
+import glob
+import random as rd
+import re
+import sys
 from pathlib import PurePath
 
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 
-import numpy as np
-import librosa
+from utils import audio_CQT
+
+AUDIO_DIR = "guitarset/audio_mono-mic/"
+ANNOT_DIR = "guitarset/annotation/"
 
 class SpectogramGenerator(keras.utils.Sequence):
 
-    def __init__(self, ids, dur_step=0.2):
+    def __init__(self, ids, dur_step=1.0, batchsize=16):
         self.ids = ids
         self.dur_step = dur_step
-        # TODO generate set of all possible 0.2-second non-overlapping windows
-        # of all files listed in 'ids'
-        # ex: [("01_id1...", 0.0), ("01_id1...", 0.2), ("01_id1...", 0.4), ...]
+        self.batchsize = batchsize
+
+        # training element ids
+        self.elems = []
+        for id in self.ids:
+            dur = librosa.get_duration(filename=AUDIO_DIR+id+"_mic.wav")
+            steps = np.arange(0, dur-0.2, 0.2)
+            self.elems += [(id, s) for s in steps]
+        self.elems = self.elems
+        id0 = self.elems[0][0]
+        # get data shape
+        x = audio_CQT(AUDIO_DIR+id0+"_mic.wav", 0, self.dur_step)
+        self.x_shape = x.shape
+        self._X_batch = np.empty((self.batchsize,) + x.shape)
 
     def __len__(self):
         """
         return number of batches in this generator
         """
-        ...
+        return len(self.elems) // self.batchsize
 
     def __getitem__(self, idx):
         """
-        get batch number `idx`
+        get batch of number `idx`
         """
-        ...
+        elems = self.elems[idx*self.batchsize:(idx+1)*self.batchsize]
+        for i,(file_id,start_sec) in enumerate(elems):
+            x = audio_CQT(AUDIO_DIR+file_id+"_mic.wav", start_sec, self.dur_step)
+            self._X_batch[i] = x
+            y = ...
+            y.append(Y)
+        return X, Y
 
     def on_epoch_end(self):
-        ...
+        rd.shuffle(self.elems)
 
 
 
@@ -42,7 +67,7 @@ def get_generators(val_split=0.1, **kwargs):
         val_split: fraction out of 1
         **kwargs: passed to the generator's init
     """
-    files = glob.glob("guitarset/audio_mono-mic/*_mic.wav")
+    files = glob.glob(ANNOT_DIR+"*.jams")
     ids = [PurePath(i).stem.strip("_mic") for i in files]
     val_size = int(len(ids) * val_split)
     test_ids = np.array([i for i in ids if i.startswith("05")])
@@ -56,4 +81,6 @@ def get_generators(val_split=0.1, **kwargs):
 
     return train_gen, val_gen, test_gen
 
-get_generators()
+tr, v, te = get_generators()
+
+print(tr[0])
--- utils.py
+++ utils.py
@@ -3,9 +3,25 @@ import pandas as pd
 
 import soundfile
 import sounddevice as sd
+import librosa
 
 def play_audio(data, samplerate):
     sd.play(data, samplerate)
     sd.wait()
 
+
+def audio_CQT(audio_path, start, dur):
+    """
+    Perform the Constant-Q Transform
+    from https://towardsdatascience.com/audio-to-guitar-tab-with-deep-learning-d76e12717f81
+    args:
+        start, dur: in seconds
+    """
+    sr = librosa.get_samplerate(audio_path)
+    data, sr = librosa.load(audio_path, sr=sr, mono=True, offset=start, duration=dur)
+    CQT = librosa.cqt(data, sr=sr, hop_length=1024, fmin=None, n_bins=96, bins_per_octave=12)
+    CQT_mag = librosa.magphase(CQT)[0]**4
+    CQT = librosa.core.amplitude_to_db(CQT_mag, ref=np.amax)
+    CQT[CQT < -60] = -120
+    return CQT
 
