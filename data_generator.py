53fc0a8ed53d98b51af68d59048a0828542c18bd
Julian Rice <julianrice@dotcomjungle.com>
2021-10-18 11:56:36 -0700

    data generator kinda working

--- data_generator.py
+++ data_generator.py
@@ -1,23 +1,29 @@
 import glob
 import random as rd
+import os
 import re
 import sys
 from pathlib import PurePath
 
 import librosa
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 
-from utils import audio_CQT
+from utils import audio_CQT, load_annot_df_from_midi, play_audio, MIDI_MAX, MIDI_MIN
 
 AUDIO_DIR = "guitarset/audio_mono-mic/"
 ANNOT_DIR = "guitarset/annotation/"
 
+
+
 class SpectogramGenerator(keras.utils.Sequence):
 
-    def __init__(self, ids, dur_step=1.0, batchsize=16):
+    def __init__(self, ids, name, dur_step=0.2, batchsize=16):
+        self.name = name
+        print("Loading", self.name, "dataset")
         self.ids = ids
         self.dur_step = dur_step
         self.batchsize = batchsize
@@ -26,14 +32,53 @@ class SpectogramGenerator(keras.utils.Sequence):
         self.elems = []
         for id in self.ids:
             dur = librosa.get_duration(filename=AUDIO_DIR+id+"_mic.wav")
-            steps = np.arange(0, dur-0.2, 0.2)
+            steps = (np.arange(0, dur-dur_step)/dur_step).astype(int)
             self.elems += [(id, s) for s in steps]
         self.elems = self.elems
-        id0 = self.elems[0][0]
+
+        # get ground-truth files
+        self.load_jams_to_df()
+
         # get data shape
+        id0 = self.elems[0][0]
         x = audio_CQT(AUDIO_DIR+id0+"_mic.wav", 0, self.dur_step)
         self.x_shape = x.shape
         self._X_batch = np.empty((self.batchsize,) + x.shape)
+        self._Y_batch = np.empty((self.batchsize, MIDI_MAX - MIDI_MIN))
+
+    def load_jams_to_df(self):
+        os.makedirs("data", exist_ok=True)
+        save_file = "data/"+self.name+"_annotations_step"+str(self.dur_step)+"_df.hdf5"
+        if os.path.exists(save_file):
+            self.annot_df = pd.read_hdf(save_file, key="df")
+        else:
+            print("  Generating ground-truth dataframe")
+            dfs = []
+            for i,file_id in enumerate(self.ids):
+                if i % 20 == 0:
+                    print(" ", i, "of", len(self.ids))
+                df = load_annot_df_from_midi(ANNOT_DIR+file_id+".jams")
+                df["round_start"] = (df["time"] / self.dur_step).round()
+                df["round_end"] = (df["end"] / self.dur_step).round()
+                df["round_midi"] = df["midi"].round().astype(int)
+                new_rows = []
+                for row in df.itertuples():
+                    steps = np.arange(row.round_start, row.round_end, self.dur_step)
+                    for step in (steps/self.dur_step):
+                        if MIDI_MIN <= row.round_midi <= MIDI_MAX:
+                            new_rows.append([
+                                int(step), row.string_num, row.round_midi
+                            ])
+                df = pd.DataFrame(new_rows, columns=["step", "string_num", "midi"])
+                df = df.sort_values("step")
+                df = df.set_index("step")
+                dfs.append(df)
+
+            full = pd.concat(dfs, axis=0, keys=self.ids)
+            full.to_hdf(save_file, key="df")
+            print("  done")
+            self.annot_df = full
+
 
     def __len__(self):
         """
@@ -46,12 +91,15 @@ class SpectogramGenerator(keras.utils.Sequence):
         get batch of number `idx`
         """
         elems = self.elems[idx*self.batchsize:(idx+1)*self.batchsize]
-        for i,(file_id,start_sec) in enumerate(elems):
-            x = audio_CQT(AUDIO_DIR+file_id+"_mic.wav", start_sec, self.dur_step)
+        self._Y_batch.fill(0)
+        for i,elem in enumerate(elems):
+            file_id, start_step = elem
+            x = audio_CQT(AUDIO_DIR+file_id+"_mic.wav", start_step*self.dur_step, self.dur_step)
             self._X_batch[i] = x
-            y = ...
-            y.append(Y)
-        return X, Y
+            if elem in self.annot_df.index:
+                notes = self.annot_df.loc[elem]
+                self._Y_batch[i][notes["midi"] - MIDI_MIN] = 1
+        return self._X_batch, self._Y_batch
 
     def on_epoch_end(self):
         rd.shuffle(self.elems)
@@ -75,12 +123,9 @@ def get_generators(val_split=0.1, **kwargs):
     np.random.shuffle(train_ids)
     val_ids, train_ids = train_ids[:val_size], train_ids[val_size:]
     
-    train_gen = SpectogramGenerator(train_ids, **kwargs)
-    val_gen = SpectogramGenerator(val_ids, **kwargs)
-    test_gen = SpectogramGenerator(test_ids, **kwargs)
+    train_gen = SpectogramGenerator(train_ids, name="train", **kwargs)
+    val_gen = SpectogramGenerator(val_ids, name="val", **kwargs)
+    test_gen = SpectogramGenerator(test_ids, name="test", **kwargs)
 
     return train_gen, val_gen, test_gen
 
-tr, v, te = get_generators()
-
-print(tr[0])
--- utils.py
+++ utils.py
@@ -3,12 +3,18 @@ import pandas as pd
 
 import soundfile
 import sounddevice as sd
+import jams
+import matplotlib.pyplot as plt
 import librosa
+
+
+MIDI_MIN = 44
+MIDI_MAX = 92
+
 
 def play_audio(data, samplerate):
     sd.play(data, samplerate)
     sd.wait()
-
 
 def audio_CQT(audio_path, start, dur):
     """
@@ -24,4 +30,57 @@ def audio_CQT(audio_path, start, dur):
     CQT = librosa.core.amplitude_to_db(CQT_mag, ref=np.amax)
     CQT[CQT < -60] = -120
     return CQT
+
+
+def load_annot_df_from_pitchcontour(filename):
+    """
+    DEPRECATED: the other version from_midi is probably preferred
+    load dataframe from a jams filename
+    returns:
+        dataframe with columns: time, freq, midi, string_num
+    """
+    jam = jams.load(filename)
+    annots = jam.search(namespace="pitch_contour")
+
+    full_df = None
+    for i,string_annot in enumerate(annots):
+        df = string_annot.to_dataframe()[["value","time"]]
+        freq = df["value"].apply(lambda x: x["frequency"])
+        midi = freq.apply(lambda x: librosa.hz_to_midi(x))
+        df = pd.concat([df["time"], freq, midi], axis=1, keys=["time", "freq", "midi"])
+        df["string_num"] = i
+        if full_df is None:
+            full_df = df
+        else:
+            full_df = pd.concat((full_df, df))
+
+    full_df = full_df[full_df["freq"] > 0]
+    full_df.sort_values(by="time", inplace=True)
+    return full_df.reset_index(drop=True)
+
+def load_annot_df_from_midi(filename):
+    """
+    load dataframe from a jams filename
+    returns:
+        dataframe with columns: time, start, end, midi
+    """
+    jam = jams.load(filename)
+    annots = jam.search(namespace="note_midi")
+
+    full_df = None
+    for i,string_annot in enumerate(annots):
+        df = string_annot.to_dataframe()
+        df = df.drop(columns=["confidence"])
+        df["end"] = df["time"] + df["duration"]
+        df["string_num"] = i
+        if full_df is None:
+            full_df = df
+        else:
+            full_df = pd.concat((full_df, df))
+
+    # full_df = full_df[full_df["freq"] > 0]
+    full_df.sort_values(by="time", inplace=True)
+    full_df.rename(columns={"value": "midi"}, inplace=True)
+    return full_df.reset_index(drop=True)
+
 
